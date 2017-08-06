80edf9c19b3cc49543388a0c43647cfe7b420cdc
John Soanes <jsoanes@viridor.co.uk>
2017-08-06 20:09:19 +0100

    Initial Commit

---
+++ swoosh-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swoosh-app
+//
+//  Created by John Soanes on 06/08/2017.
+//  Copyright © 2017 John Soanes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
